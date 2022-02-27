dab9a7f244c774376cd996c6ae3d07de2078dc23
Tyrel Jackson <tyreljackson904@gmail.com>
2022-02-27 15:48:26 -0700

    post route

--- pern-todo/server/index.js
+++ pern-todo/server/index.js
@@ -12,6 +12,18 @@ app.use(express.json())
 //Routes
 
 //create a todo
+app.post("/todos", async (req, res) => {
+    try {
+        
+        const { description } = req.body
+        const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *", [description]);
+
+        res.json(newTodo)
+
+    } catch (error) {
+        console.log(error.message)
+    }
+})
 
 //get all todos
 
