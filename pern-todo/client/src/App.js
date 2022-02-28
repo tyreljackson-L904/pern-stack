364249615331cbf62e60180f1461c6128c111432
Tyrel Jackson <tyreljackson904@gmail.com>
2022-02-28 13:24:55 -0700

    form, InputTodo

--- pern-todo/client/src/App.js
+++ pern-todo/client/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+// import InputTodo from './components/InputTodo';
+import {Form, Button} from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <h1 className='text-center mt-5'>Pern Todo List</h1>
+      <Form className='mt-5 w-50 m-auto'>
+        <Form.Group className='d-flex justify-content-center'>
+          <Form.Control type='text' placeholder='description' className='mx-5'/>
+          <Button type="submit">Add</Button>
+        </Form.Group>
+      </Form>
     </div>
   );
 }
